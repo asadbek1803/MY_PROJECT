6d2e5e6432d8477132a4cb7c8cc9e512a06aaa38
Asadbek <97333773+asadbek1803@users.noreply.github.com>
2022-01-15 12:04:17 +0500

    Add files via upload

---
+++ my_project.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 15 11:16:12 2022
+
+@author: User 4
+"""
+savol = "yaxshi ko'rgan kitobingizni kiriting "
+savol += "(dasturni to'xtatish uchun 'stop' deb yozing): "
+qiymat = ''
+while qiymat != 'stop':
+    qiymat = input(savol)
+    if qiymat != 'stop':
+        print()
+        
